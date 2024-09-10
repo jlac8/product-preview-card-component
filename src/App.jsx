a4907cb035c98505bebc8e737ee71be278cb51ed
Juan <jlalvac@uni.pe>
2024-09-09 22:28:03 -0500

    feat: add semantic html

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,31 @@ function App() {
 
   return (
     <>
-      Preview
-
-Gabrielle Essence Eau De Parfum
-
-A floral, solar and voluptuous interpretation composed by Olivier Polge,
-Perfumer-Creator for the House of CHANEL.
-
-$149.99
-$169.99
-
-Add to Cart
+      <main>
+        <article>
+          <div>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <h2>PERFUME</h2>
+            <h1>Gabrielle Essence Eau De Parfum</h1>
+            <p>A floral, solar and voluptuous
+              interpretation composed by Olivier
+              Polge, Perfumer-Creator for the House of
+              CHANEL.</p>
+            <div>
+              <span>$149.99</span>
+              <span>$169.99</span>
+            </div>
+            <div>
+              <button>
+                <img src="" alt="" />
+                Add to Cart
+              </button>
+            </div>
+          </div>
+        </article>
+      </main>
     </>
   )
 }
